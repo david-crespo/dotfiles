07ffd9ed595f61f9eb0958a199eada820c107b7b
David Crespo <david.crespo@oxidecomputer.com>
2024-05-19 17:43:14 -0500

    add helix and iterm2 files

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 #! /usr/bin/env bash
+
+# HELIX
+mkdir -p ~/.config/helix/themes
+ln -sf $PWD/helix/config.toml ~/.config/helix/config.toml
+ln -sf $PWD/helix/languages.toml ~/.config/helix/languages.toml
+ln -sf $PWD/helix/themes/ayu_evolve2.toml ~/.config/helix/themes/ayu_evolve2.toml
+
+# import iTerm color scheme manually
