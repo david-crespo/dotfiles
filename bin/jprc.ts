336a1c84cd0e86a883f8426b1f75953a67f3666f
David Crespo <crespo.dm@gmail.com>
2025-01-14 11:34:43 -0600

    add -r argument to jprc

--- bin/jprc.ts
+++ bin/jprc.ts
@@ -1,18 +1,23 @@
 #!/usr/bin/env -S deno run --allow-env --allow-read --allow-run=jj,gh
 
 import $ from "jsr:@david/dax@0.42.0"
+import { parseArgs } from "jsr:@std/cli@1.0/parse-args"
 
-await $`jj st`
+const { r } = parseArgs(Deno.args, { string: ["r"], default: { r: "@" } })
+
+await $`jj diff --stat -r ${r}`
 console.log()
 
-const bookmarks = (await $`jj bookmark list -r @ -T 'name++"\n"'`.lines()).filter(Boolean)
+const bookmarks = await $`jj bookmark list -r ${r} -T 'name++"\n"'`.lines().then((bs) =>
+  bs.filter((x) => !!x)
+)
 
 let bookmark: string
 
 if (bookmarks.length === 0) {
   bookmark = (await $.prompt("No bookmarks found. Create one:", { noClear: true })).trim()
   if (!bookmark) Deno.exit()
-  await $`jj bookmark create ${bookmark}`
+  await $`jj bookmark create ${bookmark} -r ${r}`
 } else if (bookmarks.length === 1) {
   bookmark = bookmarks[0]
 } else {
