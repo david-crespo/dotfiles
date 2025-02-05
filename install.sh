cf673c4807ff58579daabd75e2512e52cecaee17
David Crespo <crespo.dm@gmail.com>
2025-02-04 23:34:39 -0600

    use lesskey to get redraw-on-quit on Q in less

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-for f in gitconfig gitconfig-oxide githelpers gitignore jjconfig.toml tmux.conf vimrc zprofile zshrc zshenv; do
+for f in gitconfig gitconfig-oxide githelpers gitignore jjconfig.toml lesskey tmux.conf vimrc zprofile zshrc zshenv; do
   ln -sf "$PWD/$f" ~/.$f
 done
 
