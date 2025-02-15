3b928991f67470025d99b471e246e260fc2ef0b6
David Crespo <crespo.dm@gmail.com>
2025-02-14 23:57:42 -0600

    cb can pull directly from clipboard

--- bin/codeblocks.ts
+++ bin/codeblocks.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read
+#!/usr/bin/env -S deno run --allow-read --allow-env --allow-run=pbpaste
 
 /*
  * Deno script that takes a set of file paths as positional args and prints
@@ -9,6 +9,7 @@
 import { extname } from "jsr:@std/path@^1.0.8"
 import { readAll } from "jsr:@std/io@0.225.1"
 import { Command } from "jsr:@cliffy/command@1.0.0-rc.7"
+import $ from "jsr:@david/dax@0.42.0"
 
 // adoc doesn't display right in glow but it does on github
 const LANGS = ["rs", "ts", "tsx", "js", "json", "adoc", "sh"]
@@ -16,7 +17,7 @@ const LANGS = ["rs", "ts", "tsx", "js", "json", "adoc", "sh"]
 function printFile(
   filename: string,
   content: string,
-  details: boolean,
+  details: true | undefined,
   langArg: string | undefined,
 ) {
   if (details) {
@@ -41,6 +42,12 @@ function printFile(
   if (details) console.log("</details>")
 }
 
+async function getStdin() {
+  if (Deno.stdin.isTerminal()) return undefined
+  const stdin = new TextDecoder().decode(await readAll(Deno.stdin)).trim()
+  return stdin || undefined
+}
+
 await new Command()
   .name("cb")
   .description(`
@@ -55,12 +62,18 @@ on the extension. Used for piping files to my LLM CLI.`.trim())
   .example("4)", "echo \"console.log('hi')\" | cb -l js data.json")
   .arguments("[files...]")
   .option("-l, --lang <lang:string>", "Code block lang for stdin")
-  .option("-d, --details", "Wrap files in <details>", { default: false })
+  .option("-d, --details", "Wrap files in <details>")
+  .option("-p, --paste", "Pull from pbpaste")
   // TODO: add -c to wrap in XML for claude
   .action(async (opts, ...files) => {
-    if (!Deno.stdin.isTerminal()) {
-      const stdin = new TextDecoder().decode(await readAll(Deno.stdin)).trim()
-      if (stdin) printFile("[stdin]", stdin, opts.details, opts.lang)
+    const stdin = await getStdin()
+    if (stdin) printFile("[stdin]", stdin, opts.details, opts.lang)
+
+    // pull from clipboard if paste flag is passed OR automatically if
+    // there is no stdin or files
+    if (opts.paste || (!stdin && files.length === 0)) {
+      const content = await $`pbpaste`.text()
+      if (content) printFile("[clipboard contents]", content, opts.details, opts.lang)
     }
 
     for (const file of files) {
