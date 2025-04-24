808aa9c0c52644aa6e2ff66bc4ea7fd40239b8ce
David Crespo <crespo.dm@gmail.com>
2025-04-24 10:49:26 -0500

    include example comments in review command, cap log length for debug-ci

--- bin/aipr.ts
+++ bin/aipr.ts
@@ -7,7 +7,7 @@ import { Octokit } from "npm:@octokit/rest@21.1.1"
 const octokit = new Octokit()
 
 const reviewSystemPrompt =
-  "You are part of a focused, concise pull request review system. You will get the description and diff of the PR, along with full copies of the modified files for context. Review the change for correctness and convention-following. Do not reproduce the diff except in parts in order to comment to comment on that part. Do not reproduce large chunks of the diff. Focus on substantive suggestions that improve correctness or clarity. Do NOT go through the change and listing and describing what the PR does in detail unless it is relevant to a suggested change."
+  "You are part of a focused, concise pull request review system. You will get example comments to show the kind of feedback we're looking for, plus the description and diff of the PR, plus possibly more files for context. Review the change for correctness, convention-following, elegance, and good user experience. Do not reproduce the diff except in small parts in order to comment on a few lines. Do not reproduce large chunks of the diff. Focus on substantive suggestions that improve correctness or clarity. Do NOT go through the change and listing and describing what the PR does in detail unless it is relevant to a suggested change."
 
 const cb = (s: string, lang = "") => `\`\`\`${lang}\n${s}\n\`\`\``
 
@@ -45,10 +45,16 @@ const reviewCmd = new Command()
   .action(async (opts, pr) => {
     const { owner, repo } = getRepoSelector(opts.repo)
     const prContext = await getPrContext({ owner, repo, pull_number: pr })
+    // cat errors are automatically logged
+    const exampleComments = await $`cat ~/comments.txt`.text().catch(() => "")
     await $`ai -e --system ${reviewSystemPrompt}`.stdinText(
-      prContext + "Review the above change.",
+      exampleComments + prContext + "Review the above change.",
     )
   })
+
+const LOG_LINE_PREFIX = /^.+\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/g
+
+// TODO: picker for which workflow run to debug
 
 const debugCmd = new Command()
   .description("Debug recent CI failures")
@@ -66,17 +72,28 @@ const debugCmd = new Command()
       ),
     ])
 
+    // first is most recent
+    const failure = failures.at(0)
+    if (!failure) {
+      console.log("No run failures found")
+      return
+    }
+
     // using gh because I couldn't figure out how to get the logs with octokit
-    const output = (await Promise.all(
-      failures.map((f) =>
-        $`gh run view -R ${opts.repo} ${f.id} --log-failed`.text()
-          // eat errors and log them
-          .catch((e) => console.log(`Error fetching log for run ${f.id}`, e))
-      ),
-    )).join("\n\n")
+    const log = (await $`gh run view -R ${opts.repo} ${failure.id} --log-failed`.text()
+      // eat errors and log them
+      .catch((e) => {
+        console.warn(`Error fetching log for run ${failure.id}`, e)
+        return ""
+      }))
+      // remove prefixes and cap number of lines
+      .split("\n")
+      .map((line) => line.replace(LOG_LINE_PREFIX, ""))
+      .slice(-1000)
+      .join("\n")
 
     await $`ai -e 'Figure out why the diff might be causing this test failure.'`.stdinText(
-      prContext + cb(output),
+      prContext + cb(log),
     )
   })
 
