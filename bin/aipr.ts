8a5f71edc2535dc8a7f69f85231c4c03f6fb4d01
David Crespo <crespo.dm@gmail.com>
2025-04-24 17:02:26 -0500

    use gh for everything instead of octokit because of auth

--- bin/aipr.ts
+++ bin/aipr.ts
@@ -2,40 +2,38 @@
 
 import $ from "jsr:@david/dax@0.43.0"
 import { Command, ValidationError } from "jsr:@cliffy/command@1.0.0-rc.7"
-import { Octokit } from "npm:@octokit/rest@21.1.1"
-
-const octokit = new Octokit()
 
 const reviewSystemPrompt =
   "You are part of a focused, concise pull request review system. You will get example comments to show the kind of feedback we're looking for, plus the description and diff of the PR, plus possibly more files for context. Review the change for correctness, convention-following, elegance, and good user experience. Do not reproduce the diff except in small parts in order to comment on a few lines. Do not reproduce large chunks of the diff. Focus on substantive suggestions that improve correctness or clarity. Do NOT go through the change and listing and describing what the PR does in detail unless it is relevant to a suggested change."
 
 const cb = (s: string, lang = "") => `\`\`\`${lang}\n${s}\n\`\`\``
 
-type PrSelector = { owner: string; repo: string; pull_number: number }
+type PrSelector = { owner: string; repo: string; pr: number }
 
-async function getPrContext(args: PrSelector) {
+const getPrArgs = (sel: PrSelector) => $.rawArg(`-R ${sel.owner}/${sel.repo} ${sel.pr}`)
+
+async function getPrContext(sel: PrSelector) {
   const [fullPr, diff] = await Promise.all([
-    octokit.pulls.get(args),
-    octokit.pulls.get({ ...args, mediaType: { format: "diff" } }),
+    $`gh pr view ${getPrArgs(sel)}`.text(),
+    $`gh pr diff ${getPrArgs(sel)}`.text(),
   ])
-  return [
-    "# Body",
-    fullPr.data.body,
-    "# Diff",
-    cb(diff.data as unknown as string),
-  ].join("\n\n") + "\n\n"
+  return ["# Body", fullPr, "# Diff", cb(diff)].join("\n\n") + "\n\n"
 }
 
 // TODO: need PR picker for when none is specified
 
 function getRepoSelector(repoStr: string) {
-  const [owner, repo] = repoStr.split("/")
-  if (!(owner && repo)) {
-    throw new ValidationError(
-      `Bad repo selector '${repoStr}'. Must look like oxidecomputer/console.`,
-    )
+  const parts = repoStr.split("/")
+  if (parts.length === 1) {
+    return { owner: "oxidecomputer", repo: parts[0] }
+  } else if (parts.length === 2) {
+    const [owner, repo] = parts
+    return { owner, repo }
   }
-  return { owner, repo }
+
+  throw new ValidationError(
+    `Bad repo selector '${repoStr}'. Must look like 'oxidecomputer/console' or 'console'.`,
+  )
 }
 
 const reviewCmd = new Command()
@@ -44,7 +42,7 @@ const reviewCmd = new Command()
   .arguments("<pr:integer>")
   .action(async (opts, pr) => {
     const { owner, repo } = getRepoSelector(opts.repo)
-    const prContext = await getPrContext({ owner, repo, pull_number: pr })
+    const prContext = await getPrContext({ owner, repo, pr })
     // cat errors are automatically logged
     const exampleComments = await $`cat ~/comments.txt`.text().catch(() => "")
     await $`ai -e --system ${reviewSystemPrompt}`.stdinText(
@@ -56,34 +54,39 @@ const LOG_LINE_PREFIX = /^.+\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z/g
 
 // TODO: picker for which workflow run to debug
 
+type Run = { conclusion: string; databaseId: number }
+
 const debugCmd = new Command()
   .description("Debug recent CI failures")
   .option("-R,--repo <repo:string>", "Repo (owner/repo)", { required: true })
   .arguments("<pr:integer>")
   .action(async (opts, pr) => {
     const { owner, repo } = getRepoSelector(opts.repo)
-    const args = { owner, repo, pull_number: pr }
-    const ref = (await octokit.pulls.get(args)).data.head.ref
+    const sel = { owner, repo, pr }
+    const refArgs = getPrArgs(sel)
+    const ref = await $`gh pr view --json headRefName --jq .headRefName ${refArgs}`.text()
 
-    const [prContext, failures] = await Promise.all([
-      getPrContext({ owner, repo, pull_number: pr }),
-      (octokit.actions.listWorkflowRunsForRepo({ owner, repo, branch: ref })).then(
-        (resp) => resp.data.workflow_runs.filter((run) => run.conclusion === "failure"),
-      ),
+    const [prContext, runs] = await Promise.all([
+      getPrContext(sel),
+      $`gh run list -R ${owner}/${repo} -b ${ref} --json databaseId,conclusion`
+        .json<Run[]>(),
     ])
 
+    const failureIds = runs
+      .filter((r) => r.conclusion === "failure")
+      .map((f) => f.databaseId)
+
     // first is most recent
-    const failure = failures.at(0)
-    if (!failure) {
+    const failureId = failureIds.at(0)
+    if (!failureId) {
       console.log("No run failures found")
       return
     }
 
-    // using gh because I couldn't figure out how to get the logs with octokit
-    const log = (await $`gh run view -R ${opts.repo} ${failure.id} --log-failed`.text()
+    const log = (await $`gh run view -R ${owner}/${repo} ${failureId} --log-failed`.text()
       // eat errors and log them
       .catch((e) => {
-        console.warn(`Error fetching log for run ${failure.id}`, e)
+        console.warn(`Error fetching log for run ${failureId}`, e)
         return ""
       }))
       // remove prefixes and cap number of lines
@@ -103,7 +106,7 @@ const contextCmd = new Command()
   .arguments("<pr:integer>")
   .action(async (opts, pr) => {
     const { owner, repo } = getRepoSelector(opts.repo)
-    const prContext = await getPrContext({ owner, repo, pull_number: pr })
+    const prContext = await getPrContext({ owner, repo, pr })
     console.log(prContext)
   })
 
